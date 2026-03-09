973276f5a18d20a61d83299199678b47825e3c9e
Kervins Joseph <knbgtbwksp@privaterelay.appleid.com>
2026-03-09 00:11:34 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+ function videofunction() {
+        document.getElementById("MyComment").innerHTML += "A great vacation video"
+    }
+    function emailMessage() {
+        document.getElementById("MyEmail").innerHTML += " Please enter valid email address";
+    }
+    function blankMessage() {
+        document.getElementById("MyEmail").innerHTML = "Email:";
+    }
